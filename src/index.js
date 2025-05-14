db3f151558d6ea8d768438858457366c10594d52
udaykiriti <123718855+udaykiriti@users.noreply.github.com>
2025-05-14 18:09:53 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./styles.css"; // Link to the CSS file
+import "./styles.css"; 
 import App from "./App"; // Main App component
 
 ReactDOM.render(
